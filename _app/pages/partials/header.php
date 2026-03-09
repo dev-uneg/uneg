102a22127642f52e0e4618d61b899e3b6cbebf2a
dev-uneg <developer@uneg.edu.mx>
2026-03-09 11:50:49 -0600

    _tag manager google

--- _app/pages/partials/header.php
+++ _app/pages/partials/header.php
@@ -49,6 +49,15 @@ $navLink = function (string $href, string $label, string $key) use ($active, $ba
 <!doctype html>
 <html lang="es">
 <head>
+  <!-- Google Tag Manager -->
+  <script>
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NQ672S4');
+  </script>
+  <!-- End Google Tag Manager -->
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo htmlspecialchars($title); ?></title>
@@ -140,6 +149,10 @@ $navLink = function (string $href, string $label, string $key) use ($active, $ba
   </style>
 </head>
 <body class="antialiased <?php echo htmlspecialchars($bodyClass); ?>">
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NQ672S4"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
   <?php include __DIR__ . '/subheader.php'; ?>
   <?php include __DIR__ . '/nav.php'; ?>
 
